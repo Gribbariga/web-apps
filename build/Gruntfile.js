3a84f892966dafbd9108cbb37cf2872cd95d3019
Wolfgang Ginolas <wolfgang.ginolas@gwif.eu>
2023-09-11 11:34:05 +0200

    Fix build

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -776,8 +776,8 @@ module.exports = function(grunt) {
 
     grunt.registerTask('deploy-app-mobile',             []);
 
-    grunt.registerTask('deploy-app-embed',              ['embed-app-init', 'clean:prebuild', 'terser', 'less', 'copy', 'clean:postbuild']);
-    grunt.registerTask('deploy-app-test',               ['test-app-init', 'clean:prebuild', 'terser', 'less', 'copy']);
+    grunt.registerTask('deploy-app-embed',              ['embed-app-init', 'clean:prebuild', 'less', 'copy', 'clean:postbuild']);
+    grunt.registerTask('deploy-app-test',               ['test-app-init', 'clean:prebuild', 'less', 'copy']);
 
     doRegisterInitializeAppTask('common',               'Common',               'common.json');
     doRegisterInitializeAppTask('documenteditor',       'DocumentEditor',       'documenteditor.json');
